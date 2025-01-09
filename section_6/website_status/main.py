19dad3a7ab5b960d729ba2bf925c0695afc83ad7
Noel Miller <noel@noelmiller.dev>
2025-01-09 16:18:20 -0600

    removed external validators package, refactored names, added fix for SSL checking, and fixed up main function

--- section_6/website_status/main.py
+++ section_6/website_status/main.py
@@ -1,17 +1,28 @@
 #! /usr/bin/env python3
 
+import re
 import sys
+from typing import Pattern
 
 import requests
 from requests import Response
-import validators
 
+
+def validate_http_or_https_url(url: str) -> bool:
+    regex: Pattern[str] = re.compile(
+        r'^(?:http)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(regex, url) is not None
 
 def get_valid_user_input() -> str:
     while True:
         try:
             user_input: str = input("Enter a website: ")
-            if not validators.url(user_input):
+            if not validate_http_or_https_url(user_input):
                 raise ValueError("Invalid URL. Please try again.")
         except ValueError as e:
             print(e)
@@ -23,15 +34,25 @@ def get_valid_user_input() -> str:
 
 def get_response(url: str) -> Response:
     try:
-        return requests.get(url)
+        return requests.get(url, timeout=5)
+    except requests.exceptions.SSLError as e:
+        if "certificate verify failed" in str(e):
+            print("SSL certificate verification failed. Please enter a URL with a valid SSL certificate.")
+        else:
+            print(e)
+        sys.exit()
     except requests.exceptions.ConnectionError:
         print("Connection error. Please try again.")
+        sys.exit()
     except requests.exceptions.Timeout:
         print("Timeout error. Please try again.")
+        sys.exit()
     except requests.exceptions.TooManyRedirects:
         print("Too many redirects. Please try again.")
+        sys.exit()
     except requests.exceptions.RequestException:
         print("Request exception. Please try again.")
+        sys.exit()
 
 def show_response_info(response: Response) -> None:
     print("----")
@@ -45,11 +66,11 @@ def show_response_info(response: Response) -> None:
     print("----")
 
 
-def main() -> None:
+def main() -> int:
     user_input: str = get_valid_user_input()
     response: Response = get_response(user_input)
     show_response_info(response)
-    sys.exit()
+    return 0
 
 # Example Code from Tutorial, obviously I expanded on it with the above code
 #    website: str = "https://www.indently.io/abc"
@@ -57,4 +78,4 @@ def main() -> None:
 #    show_response_info(response)
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
